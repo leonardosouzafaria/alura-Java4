0aa334524f1a2f0dd015b22071dfd9ca9772cf73
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-08-31 23:44:55 -0300

    Exercicios Aula 2

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -2,28 +2,46 @@
 public class Fluxo {
 
 	public static void main(String[] args) {
-	
-		System.out.println("Início main");
-		metodoUm();
+
+		System.out.println("InÃ­cio main");
+		
+		try {
+			
+			metodoUm();
+			
+		} catch (ArithmeticException | NullPointerException e) {
+			String mensagem = e.getMessage();
+			System.out.println("Exception" + mensagem);
+		
+			System.out.println("Rastro do Exception");
+			
+			e.printStackTrace();
+		}
 		System.out.println("Fim do main");
 
 	}
-	
+
 	private static void metodoUm() {
-		
-		System.out.println("Início do metodoUM");
+
+		System.out.println("InÃ­cio do metodoUM");
 		metodoDois();
 		System.out.println("Fim do metodoUm");
-		
+
 	}
-	
+
 	private static void metodoDois() {
-		
-		System.out.println("Início do metodoDois");
-		for(int i = 0; i <= 5 ; i++) {
+
+		System.out.println("InÃ­cio do metodoDois");
+		for (int i = 0; i <= 5; i++) {
 			System.out.println(i);
+			
+			Conta c = null;
+			
+			c.deposita();
+			
+			int a = i / 0;
 		}
-		
+
 		System.out.println("Fim do metodoDois");
 	}
 }
