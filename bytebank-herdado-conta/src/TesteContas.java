10c1cd3058e1418443073cb88cf1c0e3f6b19b18
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-09-02 01:14:31 -0300

    Refatorando a classe main TesteContas usando transfere com Exception do metodo saca

--- bytebank-herdado-conta/src/TesteContas.java
+++ bytebank-herdado-conta/src/TesteContas.java
@@ -1,15 +1,21 @@
 
 public class TesteContas {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SaldoInsuficienteException{
 					
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
 		ContaPoupanca cp = new ContaPoupanca(222, 222);
 		cp.deposita(200.0);
+		try {
+			cc.transfere(120.0, cp);
+			
+		} catch(Exception ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
 		
-		cc.transfere(10.0, cp);
 		
 		System.out.println("CC: " + cc.getSaldo());
 		System.out.println("CP: " + cp.getSaldo());
